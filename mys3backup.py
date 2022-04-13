af24628ea0a021e3e885274f8f6e5d7ed9cff6b3
Cristian <cristian.medeiros@inmetrics.com.br>
2022-04-13 09:47:32 -0300

    Treating some image erros, setting upload do glacier deep archive. Code still a mess.

--- mys3backup.py
+++ mys3backup.py
@@ -11,6 +11,8 @@ import sys
 from PIL import Image
 from PIL.Image import Exif
 from PIL.ExifTags import TAGS, GPSTAGS
+from PIL import UnidentifiedImageError
+import datetime
 import requests
 import tempfile
 import shutil
@@ -18,44 +20,69 @@ import boto3
 from dotenv import load_dotenv
 
 def get_exif(file_name) -> Exif:
-    image: Image.Image = Image.open(file_name)
+    try:
+        image: Image.Image = Image.open(file_name)
+    except UnidentifiedImageError:
+        raise
+
     return image.getexif()
 
-def get_created_at(exif):
-    for tagid in exif:
-        tagname = TAGS.get(tagid, tagid)
-        value = exif.get(tagid)
-        if tagname == "DateTime":
-            break
+def get_created_at(exif, file_path):
+    if exif:
+        for tagid in exif:
+            tagname = TAGS.get(tagid, tagid)
+            value = exif.get(tagid)
+
+            if tagname == "DateTime":
+                break
+    if not exif or not tagname == "DateTime" or value.isascii():
+        # There's no metadata available, get date from file
+        value = datetime.datetime.fromtimestamp(os.path.getmtime(file_path)).strftime("%Y:%m:%d")
+
     return value
 
 def parse_created_at(input_date):
-    year = input_date[:4]
+    year = input_date[0:4]
     month = input_date[5:7]
     day = input_date[8:10]
 
     return {"year": year, "month": month, "day": day}
 
 def get_geo(exif):
-    for key, value in TAGS.items():
-        if value == "GPSInfo":
-            break
-    gps_info = exif.get_ifd(key)
-    return {
-        GPSTAGS.get(key, key): value for key, value in gps_info.items()
-    }
+    if exif:
+        for key, value in TAGS.items():
+            if value == "GPSInfo":
+                break
+
+        gps_info = exif.get_ifd(key)
+        gps_data = {
+            GPSTAGS.get(key, key): value for key, value in gps_info.items()
+        }
+
+        if 'GPSLatitude' not in gps_data:
+            gps_data = False
+      
+    else:
+        gps_data = False
+
+    return gps_data
 
 def parse_geo(latitude_ref, latitude, longitude_ref, longitude):
-    lat = float(latitude[0]) + (float(latitude[1]) / 60) + (float(latitude[2]) / 3600)
-    long = float(longitude[0]) + (float(longitude[1]) / 60) + (float(longitude[2]) / 3600)
-    if latitude_ref == "S":
-        lat_ref = "-"
-    if longitude_ref == "W":
-        long_ref = "-"
+    try:
+        lat = float(latitude[0]) + (float(latitude[1]) / 60) + (float(latitude[2]) / 3600)
+        long = float(longitude[0]) + (float(longitude[1]) / 60) + (float(longitude[2]) / 3600)
+        if latitude_ref == "S":
+            latitude_ref = "-"
+        if longitude_ref == "W":
+            longitude_ref = "-"
 
-    return_lat = "{}{}".format(lat_ref, lat)
-    return_long = "{}{}".format(long_ref, long)
-    return (return_lat, return_long)
+        return_lat = "{}{}".format(latitude_ref, lat)
+        return_long = "{}{}".format(longitude_ref, long)
+        value = (return_lat, return_long)
+    except:
+        value = False
+
+    return value
 
 def get_location(lat_long):
     latitude = lat_long[0]
@@ -63,8 +90,12 @@ def get_location(lat_long):
     APIKEY = os.getenv('openweather_apikey')
     url = "http://api.openweathermap.org/geo/1.0/reverse?lat={}&lon={}&limit=5&appid={}".format(latitude, longitude, APIKEY)
     response = requests.get(url)
-    data = response.json()
-    return data[0]["name"]
+    data = False
+    if response.status_code == 200:
+        data_json = response.json()
+        data = data_json[0]["name"]
+
+    return data
 
 def folder_path(created_at, location):
     folder = "{}/{}/{}".format(created_at["year"], created_at["month"], created_at["day"])
@@ -85,36 +116,50 @@ def s3_upload(local_directory, s3_path, bucket):
     try:
         client.head_object(Bucket=bucket, Key=s3_path)
     except:
-        client.upload_file(local_directory, bucket, s3_path)
+        client.upload_file(local_directory, bucket, s3_path, ExtraArgs={'StorageClass': 'DEEP_ARCHIVE'})
 
 def main():
     load_dotenv()
     #Le a pasta de origem
     folder = sys.argv[1]
-    folder_content = os.scandir(folder)
-    for content in folder_content:
-        # image = Image.open(content.path)
-        # print(content.path)
-        exifdata = get_exif(content.path)
-        created_at = parse_created_at(get_created_at(exifdata))
-        geo = get_geo(exifdata)
-        location = ""
-        if geo:
-            location = get_location(parse_geo(geo["GPSLatitudeRef"], geo["GPSLatitude"], geo["GPSLongitudeRef"], geo["GPSLongitude"]))
+    folder_content = os.walk(folder)
+    for dirName, subdirList, fileList in folder_content:
+        for fname in fileList:
+            origin_file_path = os.path.join(dirName, fname)
 
-        folder = folder_path(created_at, location)
-        path = os.path.join(tempfile.gettempdir(), "backup", folder)
-        file_path = os.path.join(tempfile.gettempdir(), "backup", folder, content.name)
-        s3_path = os.path.join(folder, content.name)
-        print(s3_path)
+            if os.path.splitext(origin_file_path)[-1].lower() in ['.jpg', '.jpeg', '.png']:
 
-        os.makedirs(path, exist_ok=True)
+                try:
+                    exifdata = get_exif(origin_file_path)
+                    
+                    if exifdata:
+                        data_created_at = get_created_at(exifdata, origin_file_path)
+                        created_at = parse_created_at(data_created_at)
+                    
+                        geo = get_geo(exifdata)
 
-        shutil.copy2(content.path, file_path)
-        # print(tempfile.gettempdir())
+                        location = ""
+                        if geo:
+                            geo_parse = parse_geo(geo["GPSLatitudeRef"], geo["GPSLatitude"], geo["GPSLongitudeRef"], geo["GPSLongitude"])
+                            if geo_parse:
+                                location = get_location(geo_parse)
 
-        bucket = os.getenv('bucket')
-        s3_upload(file_path, s3_path, bucket)
+                        folder = folder_path(created_at, location)
 
+                        path = os.path.join(tempfile.gettempdir(), "backup", folder)
+                        destiny_file_path = os.path.join(tempfile.gettempdir(), "backup", folder, fname)
+                        s3_path = os.path.join(folder, fname)
+                        print("Origin: {}  Destiny: {}".format(origin_file_path, destiny_file_path))
+
+                        os.makedirs(path, exist_ok=True)
+
+                        shutil.copy2(origin_file_path, destiny_file_path)
+
+                        bucket = os.getenv('bucket')
+                        s3_upload(file_path, s3_path, bucket)
+                except UnidentifiedImageError:
+                    continue
+                except:
+                    print("Erro na imagem: {} Message: {}".format(origin_file_path, sys.exc_info()))
 if __name__ == "__main__":
     main()
